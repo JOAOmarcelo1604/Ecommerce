38f4acee729a13f159c098f2c07ecd08bdcdf6bc
unknown <marcelo.oliveira@DESKTOP-LP8I4M3.gsr.local>
2025-11-17 22:41:08 -0300

    Closes #1

---
+++ src/main/java/br/com/dev/jm/comercio/eletronico/ApiParaUmEcomerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.jm.comercio.eletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParaUmEcomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParaUmEcomerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev/jm/comercio/eletronico/ApiParaUmEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.jm.comercio.eletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParaUmEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
